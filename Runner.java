f26a6254f8d83d83723bf886f1b6efd7603adccb
ppranav1123 <111915617+ppranav1123@users.noreply.github.com>
2023-01-24 13:26:00 -0800

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args){
         Game nim = new Game();
         nim.play();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Do you want to play again?");
+        String response = sc.nextLine();
+        while(response.equals("Yes")){
+            nim.play();
+            System.out.println("Do you want to play again?");
+            response = sc.nextLine();
+        }
+        System.out.println("See you next time!");
     }
 }
