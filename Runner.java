e198c264572eae1824b29a6ee941c95e14f17ce5
ppranav1123 <111915617+ppranav1123@users.noreply.github.com>
2023-01-26 13:13:09 -0800

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -8,13 +8,14 @@ public class Runner {
         Scanner sc = new Scanner(System.in);
         System.out.println("Do you want to play again?");
         String response = sc.nextLine();
-        while(response.equals("Yes")){
+
+        while(response.equals("Yes")||response.equals("yes")||
+        response.equals("Y")||response.equals("y")){
             nim.play();
             System.out.println("Do you want to play again?");
             response = sc.nextLine();
         }
         System.out.println("See you next time!");
-        sc.close();
     }
 
 }
